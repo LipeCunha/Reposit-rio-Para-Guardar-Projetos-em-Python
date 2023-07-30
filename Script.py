c5b34914402dfbce9e00412971db058e2abdcc0d
LipeCunha <140769762+LipeCunha@users.noreply.github.com>
2023-07-30 17:01:48 -0300

    Delete Script.py

--- Script.py
+++
@@ -1,4 +0,0 @@
-nome = input('Qual seu nome?')
-idade = input('Quantos anos você tem?')
-pau = input('Seu pau é em metro ou centimetro?')
-print('então seu nome é' , nome , 'e você tem a idade de' , idade , 'e seu pau se mede como?' , pau)
